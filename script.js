a455b0abcdedca1556bcd795bc646610ff085fd9
Abhishek kumar <53706631+amishramuz@users.noreply.github.com>
2022-07-18 17:49:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function new1(){
+  document.getElementById('btun1').addEventListener('click',function(){
+    document.querySelector('.bg_modal').style.display='flex';
+  });
+}
+function new2(){
+document.getElementById('close').addEventListener('click',function(){
+    document.querySelector('.bg_modal').style.display='none';
+
+});
+}
